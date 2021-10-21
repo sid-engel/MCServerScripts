bf19f5480aab59e10e9b4e0f13bd1f81a4013024
Sid Engel <me@sidengel.com>
2021-10-21 08:05:19 -0600

    Added functionality for optional compressing and keeping of uncompressed folder.
    Added functionality for optional compressing and keeping of uncompressed folder, and some notes for those who may not know what the script is for and how to use it.

--- autoworldreset.sh
+++ autoworldreset.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-# This script can easilly be adapted to work with different server managers. AMP, Multicraft, Pterodactyl, etc...
-# To set up automation make it a cronjob. Be sure to dry-run it a few times to make sure that it's functioning.
+#=============================
+# This script wil shut down the amp instance, renames the end world folder, adds a new random generated seed to the server.properties file, starts the instance back up, then optionally compresses the old folder and optionally keeps the uncompressed version.
+#=============================
+
+#============================
+# NOTE: AMP's API does not like how we do this. Later down the road, I will be changing the seed change function to go through AMP's API. However, this format of the script is easy to adapt to other server systems like Pterodactyl and Multicraft.
+
+# Also, to automate this... All you need to do is schedule it as a cron job.
+#============================
+
 
 # Variable Declaration
-# These are here to making executing the script on different Instances easier and to allow you to change the name of the world folder...
+# These are here to making executing the script on different Instances easier...
 InstanceName='SidsTestServer'
-InstanceDirectory='/home/amp/.ampdata/instances/SidsTestServer/Minecraft'
+InstanceDirectory='/home/amp/.ampdata/instances/SidTestServer/Minecraft'
 ShortSleep='sleep 3'
 AmpUsername='amp'
 Date=$(date +%F)
 OldEndName='world_the_end_OLD'
 RandomSeedGenerator=$(shuf -i 10000000000-99999999999 -n1)
+
+# At least one of these needs to be true
+KeepUncompressedFolder=true
+CreateCompressedCopy=false
 
 # Header/Welcome Script Message.
 echo ===========================================
@@ -45,3 +57,28 @@ echo "Starting Server+Instance(A New End Folder Will Generate)..."
 sudo su -c "ampinstmgr RestartInstance $InstanceName" amp
 $ShortSleep
 echo "Server+Instance Started."
+
+$ShortSleep
+
+# Create Compressed Copy If Enabled.
+if [ "$CreateCompressedCopy" = true ];
+then
+	echo "Compressing ${OldEndName}..."
+	sudo su -c "tar -cvzf ${InstanceDirectory}/BACKUP_world_the_end_${Date}.tar.gz ${InstanceDirectory}/${OldEndName}" amp
+	echo "${OldEndName} Compressed And Saved As world_the_end_${Date} in ${InstanceDirectory}."
+else
+	echo "Compressing of ${OldEndName} is disabled. Not compressing."
+fi
+
+# Delete Uncompressed Folder If Enabled.
+if [ "$KeepUncompressedFolder" = true ];
+then
+	echo "Keeping Uncompressed Folder."
+else
+	echo "Removing Uncompressed Folder..."
+	sudo su -c "rm -r ${InstanceDirectory}/${OldEndName}" amp
+	echo "Uncompressed Folder Removed."
+fi
+echo "Script Ran Successfully. Exiting..."
+
+$ShortSleep
